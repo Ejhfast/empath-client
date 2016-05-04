d9f2f083b77c5d1c775a27303c56418614c1af6a
Ethan Fast <ethanfast@Ethans-MacBook-Air-2.local>
2016-05-04 13:45:51 -0700

    readme

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'empath',
   packages = ['empath'], # this must be the same as the name above
-  version = '0.05',
+  version = '0.07',
   description = 'A tool for text analysis',
   author = 'Ethan Fast',
   author_email = 'ejhfast@gmail.com',
   url = 'https://github.com/Ejhfast/empath-client', # use the URL to the github repo
-  download_url = 'https://github.com/Ejhfast/meta/empath-client/0.3',
+  download_url = 'https://github.com/Ejhfast/meta/empath-client/0.31',
   keywords = ['social science', 'lexicon', 'text analysis'], # arbitrary keywords
   package_data= { 'empath': ['data/categories.tsv', "data/user/blank"]},
   classifiers = [],
